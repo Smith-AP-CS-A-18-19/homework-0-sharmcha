7e241e211762b700e25382b71bca07c220586afb
Chase Smith <mcsmith01@alumni.amherst.edu>
2018-05-08 13:29:55 -0400

    Added instructions and main method

--- Homework0.java
+++ Homework0.java
@@ -1,3 +1,14 @@
-public class Homework2 {
+/*
+ * First and Last Names
+ */
 
+public class Homework0 {
+
+	/*
+	 * Print out the line "Hello, World!"
+	 * Ensure that you match case and punctuation exactly
+	 */
+	public static void main(String[] args) {
+
+	}
 }
